867866f3be7eb94b6f14e87321dd0c46ff716c50
salmansayanidev <salmansayani822@gmail.com>
2024-08-16 02:56:02 +0500

    new

---
+++ footer.php
@@ -0,0 +1,195 @@
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="text-mb-center footer-logo-area">
+                        <a class="text-decoration-none" href="#">
+                            <img class="img-fluid footer-logo" src="./assets/images/logo.png" alt="">
+                        </a>
+                        <p class="footer-para mt-3 mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <ul class="social-icons-list">
+                            <li class="social-icon-area">
+                                <a class="social-icon-link" href="#">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li class="social-icon-area">
+                                <a class="social-icon-link" href="#">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li class="social-icon-area">
+                                <a class="social-icon-link" href="#">
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="social-icon-area">
+                                <a class="social-icon-link" href="#">
+                                    <i class="fa-brands fa-youtube"></i>
+                                </a>
+                            </li>
+                            <li class="social-icon-area">
+                                <a class="social-icon-link" href="#">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h3 class="footer-title text-gradient mt-4"> Quick Links </h3>
+                    <ul class="footer-link-list">
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>market data</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Elite Trading Alerts</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Education Trading Courses</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Knowledge </span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Resources/Tools</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Our Blogs</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h3 class="footer-title text-gradient mt-4"> Helpful Links </h3>
+                    <ul class="footer-link-list">
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Contact Us</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Help And Advice</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>FAQ’s</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Terms And Conditions</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link" href="#">
+                                <span class="angle mustard-yello font-weight-900"> > </span>
+                                <span>Privacy Policy</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h3 class="footer-title text-gradient mt-4"> Get in Touch </h3>
+                    <ul class="footer-link-list">
+                        <li class="">
+                            <a class="text-decoration-none footer-link gap-4" href="tel:(754) 715-2475">
+                                <span class="angle mustard-yello font-weight-900"> 
+                                    <i class="fa-solid fa-phone"></i>    
+                                </span>
+                                <span>(754) 715-2475</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a class="text-decoration-none footer-link gap-4" href="mailto:jccarrasquillab_15@yahoo.com">
+                                <span class="angle mustard-yello font-weight-900"> 
+                                    <i class="fa-solid fa-envelope"></i>    
+                                </span>
+                                <span>jccarrasquillab_15@yahoo.com</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <address class="text-decoration-none footer-link gap-4">
+                                <span class="angle mustard-yello font-weight-900"> 
+                                    <i class="fa-solid fa-location-dot"></i>    
+                                </span>
+                                <span>Coral Springs, Florida,33076.</span>
+                            </address>
+                        </li>
+                    </ul>
+                    <div class="footer-img-area">
+                        <img class="img-fluid mt-4" src="./assets/images/footer-img.png" alt="">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <hr class="footer-hr">
+                </div>
+                <div class="col-12">
+                    <div class="footer-btm-area">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <p class="footer-btm-para text-white">Copyright 2024 © <span class="font-weight-900 mustard-yello">Flex Trading</span>. All rights reserved.</p>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <p class="footer-btm-para text-white text-end">Design & Developed By: <a href="https://www.webdesignglory.com/" target="_blank" class="text-decoration-none font-weight-900 mustard-yello">Web Design Glory</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <div class="search-popup-wrap">
+        <button class="search-cls-btn search-popup-btn" type="button">
+            <i class="fa-solid fa-xmark"></i>
+        </button>
+        <div class="search-popup">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-8">
+                    <form action="">
+                        <div class="newsletter-field-area bg-white">
+                            <input type="text" required placeholder="Search Here" class="input-field text-black" name="" id="">
+                            <button class="submit-btn" type="submit">
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script is:inline type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script src="./assets/js/index.js"></script>
+
+    </body>
+</html>
